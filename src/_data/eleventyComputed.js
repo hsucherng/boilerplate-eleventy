0d2a71a1703dbfcb6cf6c33f691c3ba8f167c763
Hsu-Cherng <hsucherng.chong@gmail.com>
2021-06-14 10:42:35 +0800

    Globally adjust the permalink so that it does not output files as folder index HTMLs

--- src/_data/eleventyComputed.js
+++ src/_data/eleventyComputed.js
@@ -3,5 +3,13 @@ module.exports = {
     return data.page.url
       .split('/')
       .reduce((a, b) => a+(b && '../'),)
-  }
+  },
+
+  /*
+   * Globally adjust the permalink so that it does not output
+   * files as folder index HTMLs.
+   *
+   * https://github.com/11ty/eleventy/issues/1199
+   */
+  permalink: (data) => data.page.filePathStem + '.html'
 };
